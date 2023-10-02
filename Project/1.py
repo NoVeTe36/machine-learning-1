614fad01b5833b708aabd1519acd47302d4f9022
NoVeTe36 <tungnv.bi12-480@st.usth.edu.vn>
2023-10-03 00:41:56 +0700

    update model

--- Project/1.py
+++ Project/1.py
@@ -1,12 +1,4 @@
-#  import http requests
-import requests
-#  import beautiful soup
-from bs4 import BeautifulSoup
+import librosa   
+y, s = librosa.load('./left_nvt.wav', sr=16000) 
 
-
-url = "https://play.stream4.me"
-
-#  get the html content
-html_content = requests.get(url).text
-
-print(html_content)
+print(y.shape)
